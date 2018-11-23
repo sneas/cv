333051aa42f06e0402d4a1212701241ff6e5efe6
Dima Snisarenko <snisarenkodima@gmail.com>
2018-11-23 20:53:19 +0100

    Git version

--- .circleci/gh-pages.sh
+++ .circleci/gh-pages.sh
@@ -3,7 +3,7 @@
 echo "GH_NAME $GH_NAME"
 echo "GH_EMAIL $GH_EMAIL"
 
-git status
+git --version
 
 [ ! -z "$GH_NAME" ] && git config --global user.name $GH_NAME
 [ ! -z "$GH_EMAIL" ] && git config --global user.email $GH_EMAIL
