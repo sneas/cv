323f76a13eed63da111b6d588d6f2a1802807b9d
Dima Snisarenko <snisarenkodima@gmail.com>
2018-11-23 21:00:20 +0100

    Debug

--- .circleci/gh-pages.sh
+++ .circleci/gh-pages.sh
@@ -3,7 +3,8 @@
 [ ! -z "$GH_NAME" ] && git config --global user.name $GH_NAME
 [ ! -z "$GH_EMAIL" ] && git config --global user.email $GH_EMAIL
 
+echo "User email"
 
-git config user.name
+git config --global user.email
 
 npm run gh
