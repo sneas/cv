fcab80916f5e64cb2b2086c2a89ef478e7ab4beb
Dima Snisarenko <snisarenkodima@gmail.com>
2018-11-23 21:04:48 +0100

    Display error when GH_NAME and GH_EMAIL variables are not set

--- .circleci/gh-pages.sh
+++ .circleci/gh-pages.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-[ ! -z "$GH_NAME" ] && git config --global user.name "$GH_NAME"
-[ ! -z "$GH_EMAIL" ] && git config --global user.email "$GH_EMAIL"
+if [[ -z "$GH_NAME" || -z "$GH_EMAIL" ]]; then
+ echo "GH_NAME and GH_EMAIL variables must be set"
+ exit 1
+fi
 
-echo "User email ($GH_EMAIL)"
-git config --global user.email
-
-echo "User name ($GH_NAME)"
-git config --global user.name
+git config --global user.name "$GH_NAME"
+git config --global user.email "$GH_EMAIL"
 
 npm run gh
