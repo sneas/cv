c4e84c1ddf7e4570e6e817dd43aab51a9cac0918
Dima Snisarenko <snisarenkodima@gmail.com>
2026-08-10 15:22:49 +0200

    fix: typo

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -36,7 +36,7 @@ module.exports = {
       duration: '5 years',
       skills: ['AWS', 'GCP', 'Terraform', 'NodeJS', 'React Native', 'Angular', 'OAuth', 'Browser Extensions', 'TypeScript'],
       contents: `
-As the only founder and software engineer of an [language-learning platform](https://vocably.pro), I am fully responsible for the entire product lifecycle. From ideation to revenue generation including customer support, marketing, and GDPR compliance. The platform is growing slowly, but steadily without much babysitting on my side. The platform includes:
+As the only founder and software engineer of a [language-learning platform](https://vocably.pro), I am fully responsible for the entire product lifecycle. From ideation to revenue generation including customer support, marketing, and GDPR compliance. The platform is growing slowly, but steadily without much babysitting on my side. The platform includes:
 
 - AWS/GCP cost-optimized infrastructure that is running AI prompts and caching the results 
 - iOS and Android apps created with React Native
